88c8aaee5e734ef997488943edff064a9dac035e
Pritam Mandal <pritampmandal@gmail.com>
2025-09-27 15:25:47 +0530

    first commit

---
+++ components/home/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <>
+      <div className='w-full h-screen '>
+        This is the hero Section
+      </div>
+    </>
+  );
+}
+
+export default Hero;
