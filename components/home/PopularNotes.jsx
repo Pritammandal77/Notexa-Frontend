7c393870533e54dc36b5cd42a7164865434518eb
Pritam Mandal <pritampmandal@gmail.com>
2025-11-06 13:17:06 +0530

    header bug fixed

--- components/home/PopularNotes.jsx
+++ components/home/PopularNotes.jsx
@@ -10,7 +10,7 @@ function PopularNotes() {
 
     return (
         <>
-            <div className='w-full h-auto flex flex-col gap-10 py-15'>
+            <div className='w-[100vw] h-auto flex flex-col gap-10 py-15 overflow-x-hidden'>
                 <div className='flex items-center justify-start xl:px-30'>
                     <h1 className="text-5xl md:text-5xl font-extrabold font-playfair text-gray-800 leading-tight">
                         <span className="text-orange-500">Popular</span> Notes
