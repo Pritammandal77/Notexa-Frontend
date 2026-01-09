be90a6ea0b406d258e13b39148b1a69975e05a21
Pritam Mandal <pritampmandal@gmail.com>
2026-01-09 16:53:18 +0530

    fetched the create wallet API

--- app/(user)/dashboard/page.js
+++ app/(user)/dashboard/page.js
@@ -1,6 +1,19 @@
-import React from 'react';
+"use client"
+
+import { createOrFetchWallet } from '@/utils/payoutApi';
+import React, { useEffect } from 'react';
 
 function page() {
+
+    useEffect(() => {
+        const getWallet = async () => {
+            const res = await createOrFetchWallet()
+            console.log(res.data.data)
+        }
+
+        getWallet();
+    }, []);
+
     return (
         <>
             <div className='w-full min-h-screen h-auto'>
--- utils/payoutApi.js
+++ utils/payoutApi.js
@@ -19,3 +19,12 @@ export const createNewTransaction = async (notesId, paymentId) => {
 
     return res;
 }
+
+export const createOrFetchWallet = async () => {
+    try {
+        const res = await axiosInstance.get("/api/v1/payout/create-or-fetch-wallet")
+        return res;
+    } catch (error) {
+        console.log("error while fetching wallet")
+    }
+}
